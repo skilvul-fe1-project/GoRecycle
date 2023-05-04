36bd57de9bf43fe92779fa3a994f6fe53a2703a5
figofer <figoferdian18@gmail.com>
2023-05-04 22:26:43 +0700

    Menambahkan fungsi js

--- main.js
+++ main.js
@@ -9,3 +9,11 @@ function handleScroll() {
 }
 
 window.addEventListener("scroll", handleScroll);
+
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("active");
+})
