70e10b0a23f5699841987c2792d6b71729d08ee5
Brian Adriansyah <brianadri45@gmail.com>
2023-05-05 09:35:47 +0700

    edit navbar responsif

--- artikel/script.js
+++ artikel/script.js
@@ -27,3 +27,23 @@ async function renderData() {
 }
 
 renderData()
+
+const header = document.querySelector("header");
+
+function handleScroll() {
+    if (window.scrollY > 0) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+window.addEventListener("scroll", handleScroll);
+
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("active");
+})
